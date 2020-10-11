b2cfa055d0acebc6eef80c421ada01f375770503
J Sanderson <ju.a.sanderson@gmail.com>
2020-10-11 10:12:38 +0100

    feat: add style, more details to user list

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h2>Users</h2>
-    <ul>
-        @foreach($users as $user)
-            <li>
-                <a href="{{ route('users.show', $user->id) }}">{{ $user->name }}</a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="intro">
+        <h2>Users</h2>
+    </div>
+
+    <div class="user-list">
+        <ul>
+            @foreach($users as $user)
+                <li>
+                    <a href="{{ route('users.show', $user->id) }}">{{ $user->name }}</a> ~ {{ count($user->images) }} 
+                    @if( count($user->images) === 1 )
+                        image
+                    @else
+                        images
+                    @endif
+                     uploaded
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
