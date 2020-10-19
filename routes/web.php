43218e2cab6fe412efc29100b6f665ca421b12b1
J Sanderson <ju.a.sanderson@gmail.com>
2020-10-19 14:11:42 +0100

    feat: restrict routes to verified users

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,16 @@ Route::get('/', function () {
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-Route::post('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::post('/users/{id}', [UserController::class, 'update'])->name('users.update')->middleware('verified');
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 
 Route::get('/latest', [ImageController::class, 'index'])->name('images.index');
-Route::get('/images/upload', [ImageController::class, 'create'])->name('images.create')->middleware('auth');
-Route::post('/images/upload', [ImageController::class, 'store'])->name('images.store')->middleware('auth');
-Route::get('/images/edit/{id}', [ImageController::class, 'edit'])->name('images.edit')->middleware('auth');
-Route::put('/images/edit/{id}', [ImageController::class, 'update'])->name('images.update')->middleware('auth');
-Route::delete('/images/{id}', [ImageController::class, 'destroy'])->name('images.destroy')->middleware('auth');
+Route::get('/images/upload', [ImageController::class, 'create'])->name('images.create')->middleware('verified');
+Route::post('/images/upload', [ImageController::class, 'store'])->name('images.store')->middleware('verified');
+Route::get('/images/edit/{id}', [ImageController::class, 'edit'])->name('images.edit')->middleware('verified');
+Route::put('/images/edit/{id}', [ImageController::class, 'update'])->name('images.update')->middleware('verified');
+Route::delete('/images/{id}', [ImageController::class, 'destroy'])->name('images.destroy')->middleware('verified');
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
